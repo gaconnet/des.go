bc9a6fdbd2d1390a38d0bf49eba2838b9c47963f
CJ Gaconnet <gaconnet+github@gmail.com>
2010-12-17 11:07:03 -0600

    Ran all Go source files through gofmt
    * Ran all Go source files through gofmt with default options

--- src/pkg/crypto/des/cipher.go
+++ src/pkg/crypto/des/cipher.go
@@ -1,28 +1,27 @@
-
 package des
 
 import (
-  "os"
-  "strconv"
+	"os"
+	"strconv"
 )
 
-type DES struct{
-  enc [] uint32
-  dec [] uint32
+type DES struct {
+	enc []uint32
+	dec []uint32
 }
 
 type DES2 struct {
-  des3 *DES3
+	des3 *DES3
 }
 
 type DES3 struct {
-  des1, des2, des3 *DES
+	des1, des2, des3 *DES
 }
 
-type KeySizeError   int
+type KeySizeError int
 type KeyParityError int
 
-func (k KeySizeError)   String() string {
+func (k KeySizeError) String() string {
 	return "crypto/des: invalid key size " + strconv.Itoa(int(k))
 }
 func (k KeyParityError) String() string {
@@ -35,87 +34,89 @@ func (k KeyParityError) String() string {
 
 func NewDESCipher(key []byte) (*DES, os.Error) {
 
-  if nil == key || 8 != len(key) {
-    return nil, KeySizeError(len(key))
-  }
+	if nil == key || 8 != len(key) {
+		return nil, KeySizeError(len(key))
+	}
 
-  if which, ok := checkParityBytes(key); ok != true {
-    return nil, KeyParityError(which)
-  }
+	if which, ok := checkParityBytes(key); ok != true {
+		return nil, KeyParityError(which)
+	}
 
-  c := &DES{make([]uint32, 32), make([]uint32, 32)}
-  deskey(key, c)
+	c := &DES{make([]uint32, 32), make([]uint32, 32)}
+	deskey(key, c)
 
-  return c, nil
+	return c, nil
 }
 func NewDES2Cipher(key []byte) (*DES2, os.Error) {
-  if nil == key || 16 != len(key) {
-    return nil, KeySizeError(len(key))
-  }
+	if nil == key || 16 != len(key) {
+		return nil, KeySizeError(len(key))
+	}
 
-  if des3, err := newDES3Cipher(key); err != nil {
-    return nil, err
-  } else {
-    return &DES2{des3}, nil
-  }
-  // can't happen.
-  // compiler "bug" complains about no return stmt.
-  return nil, nil 
+	if des3, err := newDES3Cipher(key); err != nil {
+		return nil, err
+	} else {
+		return &DES2{des3}, nil
+	}
+	// can't happen.
+	// compiler "bug" complains about no return stmt.
+	return nil, nil
 }
 
 func NewDES3Cipher(key []byte) (*DES3, os.Error) {
-   if nil == key || 24 != len(key) {
-    return nil, KeySizeError(len(key))
-  }
+	if nil == key || 24 != len(key) {
+		return nil, KeySizeError(len(key))
+	}
 
-  return newDES3Cipher(key)
+	return newDES3Cipher(key)
 }
 
 func newDES3Cipher(key []byte) (*DES3, os.Error) {
 
-  var des  [3]*DES
-  var keys [3][]byte
+	var des [3]*DES
+	var keys [3][]byte
 
-  // this creates both 2DES and 3DES ciphers,
-  // key length & parity checks are performed in the
-  // public methods.
-  //
-  // no sanity length checks, this is an internal method and will lead
-  // to out-of-bounds in case it's abused internally.
+	// this creates both 2DES and 3DES ciphers,
+	// key length & parity checks are performed in the
+	// public methods.
+	//
+	// no sanity length checks, this is an internal method and will lead
+	// to out-of-bounds in case it's abused internally.
 
-  for i := range(keys) {
-    switch (i) {
-      case 0: keys[0] = key[0:8]
-      case 1: keys[1] = key[8:16]
-      case 2:
-        if len(key) == 16 {
-          keys[2] = keys[0]
-        } else {
-          keys[2] = key[16:]
-        }
-      // default panic?
-    } //switch
+	for i := range keys {
+		switch i {
+		case 0:
+			keys[0] = key[0:8]
+		case 1:
+			keys[1] = key[8:16]
+		case 2:
+			if len(key) == 16 {
+				keys[2] = keys[0]
+			} else {
+				keys[2] = key[16:]
+			}
+			// default panic?
+		} //switch
 
-    var err os.Error
-    if des[i], err = NewDESCipher(keys[i]); err != nil {
-      return nil, err
-    }
-  }
+		var err os.Error
+		if des[i], err = NewDESCipher(keys[i]); err != nil {
+			return nil, err
+		}
+	}
 
-  return &DES3{des[0],des[1], des[2]}, nil
+	return &DES3{des[0], des[1], des[2]}, nil
 
 }
 
 
 // BlockSize returns the cipher's block size.
 func (c *DES) BlockSize() int {
-  return 8
+	return 8
 }
 func (c *DES2) BlockSize() int {
-  return 8
+	return 8
 }
 func (c *DES3) BlockSize() int {
-  return 8
+	return 8
 }
 
 // Encrypt encrypts the first block in src into dst.
@@ -123,30 +124,29 @@ func (c *DES3) BlockSize() int {
 // function works in place, though you probably wouldn't
 // use it directly anyhow.
 func (c *DES) Encrypt(dst, src []byte) {
-  desfunc(dst, src, c.enc)
+	desfunc(dst, src, c.enc)
 }
 func (c *DES3) Encrypt(dst, src []byte) {
-  desfunc(dst, src, c.des1.enc)
-  desfunc(dst, dst, c.des2.dec)
-  desfunc(dst, dst, c.des3.enc)
+	desfunc(dst, src, c.des1.enc)
+	desfunc(dst, dst, c.des2.dec)
+	desfunc(dst, dst, c.des3.enc)
 }
 func (c *DES2) Encrypt(dst, src []byte) {
-  c.des3.Encrypt(dst, src)
+	c.des3.Encrypt(dst, src)
 }
 
 
 // Decrypt decrypts the first block in src into dst.
 // Src and dst may point at the same memory.
 func (c *DES) Decrypt(dst, src []byte) {
-  desfunc(dst, src, c.dec)
+	desfunc(dst, src, c.dec)
 }
 func (c *DES3) Decrypt(dst, src []byte) {
-  desfunc(dst, src, c.des3.dec)
-  desfunc(dst, dst, c.des2.enc)
-  desfunc(dst, dst, c.des1.dec)
+	desfunc(dst, src, c.des3.dec)
+	desfunc(dst, dst, c.des2.enc)
+	desfunc(dst, dst, c.des1.dec)
 }
 
 func (c *DES2) Decrypt(dst, src []byte) {
-  c.des3.Decrypt(dst, src)
+	c.des3.Decrypt(dst, src)
 }
-
--- src/pkg/crypto/des/des_test.go
+++ src/pkg/crypto/des/des_test.go
@@ -1,10 +1,10 @@
 package des
 
 import (
-  "testing"
-  //"fmt"
-  "crypto/subtle"
-  "crypto/block"
+	"testing"
+	//"fmt"
+	"crypto/subtle"
+	"crypto/block"
 )
 
 /*
@@ -12,253 +12,248 @@ import (
  * Key    : 0123 4567 89ab cdef
  * Plain  : 0123 4567 89ab cde7
  * Cipher : c957 4425 6a5e d31d
-*/
+ */
 
 var (
-  key   = []byte {0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef}
-  plain = []byte {0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xe7}
-  ciphr = []byte {0xc9, 0x57, 0x44, 0x25, 0x6a, 0x5e, 0xd3, 0x1d}
+	key   = []byte{0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef}
+	plain = []byte{0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xe7}
+	ciphr = []byte{0xc9, 0x57, 0x44, 0x25, 0x6a, 0x5e, 0xd3, 0x1d}
 
-  key2  = []byte {0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10}
-  ciphr2= []byte {0x7f, 0x1d, 0x0a, 0x77, 0x82, 0x6b, 0x8a, 0xff}
+	key2   = []byte{0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10}
+	ciphr2 = []byte{0x7f, 0x1d, 0x0a, 0x77, 0x82, 0x6b, 0x8a, 0xff}
 
-  key3  = []byte {0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10, 0x89, 0xab, 0xcd, 0xef, 0x01, 0x23, 0x45, 0x67}
-  ciphr3= []byte {0xde, 0x0b, 0x7c, 0x06, 0xae, 0x5e, 0x0e, 0xd5}
-
-
-  key_parity_error = []byte {0x01, 0x23, 0x45, 0x67, 0x89, 0xac, 0xcd, 0xef}
+	key3   = []byte{0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10, 0x89, 0xab, 0xcd, 0xef, 0x01, 0x23, 0x45, 0x67}
+	ciphr3 = []byte{0xde, 0x0b, 0x7c, 0x06, 0xae, 0x5e, 0x0e, 0xd5}
 
 
-  oddParityBytes = []byte {0x01, 0x02, 0x04, 0x07, 0xfe}
-  evenParityBytes = []byte {0x00, 0x03, 0x05, 0x06, 0xff}
+	key_parity_error = []byte{0x01, 0x23, 0x45, 0x67, 0x89, 0xac, 0xcd, 0xef}
 
-  // Bulk Tests
-  //des1keys = [][]byte{}
-  //plainTextBlocks = [][]byte{}
-  //des1ciphers = [][][]byte{}
- 
+
+	oddParityBytes  = []byte{0x01, 0x02, 0x04, 0x07, 0xfe}
+	evenParityBytes = []byte{0x00, 0x03, 0x05, 0x06, 0xff}
+
+	// Bulk Tests
+	//des1keys = [][]byte{}
+	//plainTextBlocks = [][]byte{}
+	//des1ciphers = [][][]byte{}
+
 
 )
 
-func TestEnc(t *testing.T){
-  c, _ := NewDESCipher(key)
-  result := make([]byte,8)
-  c.Encrypt(result, plain)
+func TestEnc(t *testing.T) {
+	c, _ := NewDESCipher(key)
+	result := make([]byte, 8)
+	c.Encrypt(result, plain)
 
-  if 1 != subtle.ConstantTimeCompare(result,ciphr) {
-    t.Errorf("1 DES Enc failed.")
-  }
+	if 1 != subtle.ConstantTimeCompare(result, ciphr) {
+		t.Errorf("1 DES Enc failed.")
+	}
 
-  c2,_ := NewDES2Cipher(key2)
-  c2.Encrypt(result, plain)
+	c2, _ := NewDES2Cipher(key2)
+	c2.Encrypt(result, plain)
 
-  if 1 != subtle.ConstantTimeCompare(result,ciphr2) {
-    t.Errorf("2 DES Enc failed.")
-  }
+	if 1 != subtle.ConstantTimeCompare(result, ciphr2) {
+		t.Errorf("2 DES Enc failed.")
+	}
 
-  c3,_ := NewDES3Cipher(key3)
-  c3.Encrypt(result, plain)
+	c3, _ := NewDES3Cipher(key3)
+	c3.Encrypt(result, plain)
 
-  if 1 != subtle.ConstantTimeCompare(result,ciphr3) {
-    t.Errorf("3 DES Enc failed.")
-  }
+	if 1 != subtle.ConstantTimeCompare(result, ciphr3) {
+		t.Errorf("3 DES Enc failed.")
+	}
 }
 
 // fairly braindead stress test, just to make sure ...
 func TestEncBulk(t *testing.T) {
-  for i, key := range(des1keys) {
-    if cipher, err := NewDESCipher(key); nil == err {
-        testEncBulk(t, i, cipher, des1ciphers)
-    } else {
-      t.Errorf("1 DES #%d failed with err: %s", i, err)
-    }
-  }
+	for i, key := range des1keys {
+		if cipher, err := NewDESCipher(key); nil == err {
+			testEncBulk(t, i, cipher, des1ciphers)
+		} else {
+			t.Errorf("1 DES #%d failed with err: %s", i, err)
+		}
+	}
 
-  for i, key := range(des2keys) {
-    if cipher, err := NewDES2Cipher(key); nil == err {
-      testEncBulk(t, i, cipher, des2ciphers)
-    } else {
-      t.Errorf("2 DES #%d failed with err: %s", i, err)
-    }
-  }
-  // yes, generics WOULD be nice...
-  for i, key := range(des3keys) {
-    if cipher, err := NewDES3Cipher(key); nil == err {
-      testEncBulk(t, i, cipher, des3ciphers)
-    } else {
-      t.Errorf("3 DES #%d failed with err: %s", i, err)
-    }
-  }
+	for i, key := range des2keys {
+		if cipher, err := NewDES2Cipher(key); nil == err {
+			testEncBulk(t, i, cipher, des2ciphers)
+		} else {
+			t.Errorf("2 DES #%d failed with err: %s", i, err)
+		}
+	}
+	// yes, generics WOULD be nice...
+	for i, key := range des3keys {
+		if cipher, err := NewDES3Cipher(key); nil == err {
+			testEncBulk(t, i, cipher, des3ciphers)
+		} else {
+			t.Errorf("3 DES #%d failed with err: %s", i, err)
+		}
+	}
 }
 
 // fairly braindead stress test, just to make sure ...
 func TestDecBulk(t *testing.T) {
-  for i, key := range(des1keys) {
-    if cipher, err := NewDESCipher(key); nil == err {
-        testDecBulk(t, i, cipher, des1ciphers)
-    } else {
-      t.Errorf("1 DES #%d failed with err: %s", i, err)
-    }
-  }
-  for i, key := range(des2keys) {
-    if cipher, err := NewDES2Cipher(key); nil == err {
-      testDecBulk(t, i, cipher, des2ciphers)
-    } else {
-      t.Errorf("2 DES #%d failed with err: %s", i, err)
-    }
-  }
-  // yes, generics WOULD be nice...
-  for i, key := range(des3keys) {
-    if cipher, err := NewDES3Cipher(key); nil == err {
-      testDecBulk(t, i, cipher, des3ciphers)
-    } else {
-      t.Errorf("3 DES #%d failed with err: %s", i, err)
-    }
-  }
+	for i, key := range des1keys {
+		if cipher, err := NewDESCipher(key); nil == err {
+			testDecBulk(t, i, cipher, des1ciphers)
+		} else {
+			t.Errorf("1 DES #%d failed with err: %s", i, err)
+		}
+	}
+	for i, key := range des2keys {
+		if cipher, err := NewDES2Cipher(key); nil == err {
+			testDecBulk(t, i, cipher, des2ciphers)
+		} else {
+			t.Errorf("2 DES #%d failed with err: %s", i, err)
+		}
+	}
+	// yes, generics WOULD be nice...
+	for i, key := range des3keys {
+		if cipher, err := NewDES3Cipher(key); nil == err {
+			testDecBulk(t, i, cipher, des3ciphers)
+		} else {
+			t.Errorf("3 DES #%d failed with err: %s", i, err)
+		}
+	}
 
 }
 
 
 func testEncBulk(t *testing.T, kIdx int, cipher block.Cipher, cipherTexts [][][]byte) {
-  result := make([]byte,8)
-  for j, plain := range(plainTextBlocks) {
-    cipher.Encrypt(result, plain)
-    if 1 != subtle.ConstantTimeCompare(result, cipherTexts[j][kIdx]) {
-      t.Errorf("? DES #%d encryption failed with plaintext #%d", j, kIdx)
-    }
-  }
+	result := make([]byte, 8)
+	for j, plain := range plainTextBlocks {
+		cipher.Encrypt(result, plain)
+		if 1 != subtle.ConstantTimeCompare(result, cipherTexts[j][kIdx]) {
+			t.Errorf("? DES #%d encryption failed with plaintext #%d", j, kIdx)
+		}
+	}
 }
 
 func testDecBulk(t *testing.T, kIdx int, cipher block.Cipher, cipherTexts [][][]byte) {
-  result := make([]byte,8)
-  for j, plain := range(plainTextBlocks) {
-    cipher.Decrypt(result, cipherTexts[j][kIdx])
-    if 1 != subtle.ConstantTimeCompare(result, plain) {
-      t.Errorf("? DES #%d decrypt failed with plaintext #%d", j, kIdx)
-    }
-  }
+	result := make([]byte, 8)
+	for j, plain := range plainTextBlocks {
+		cipher.Decrypt(result, cipherTexts[j][kIdx])
+		if 1 != subtle.ConstantTimeCompare(result, plain) {
+			t.Errorf("? DES #%d decrypt failed with plaintext #%d", j, kIdx)
+		}
+	}
 }
 
 
-func TestDec(t *testing.T){
-  c, _ := NewDESCipher(key)
-  result := make([]byte,8)
-  c.Decrypt(result, ciphr)
+func TestDec(t *testing.T) {
+	c, _ := NewDESCipher(key)
+	result := make([]byte, 8)
+	c.Decrypt(result, ciphr)
 
-  if 1 != subtle.ConstantTimeCompare(result,plain) {
-    t.Errorf("1 DES Dec failed.")
-  }
+	if 1 != subtle.ConstantTimeCompare(result, plain) {
+		t.Errorf("1 DES Dec failed.")
+	}
 
-  c2,_ := NewDES2Cipher(key2)
-  c2.Decrypt(result, ciphr2)
+	c2, _ := NewDES2Cipher(key2)
+	c2.Decrypt(result, ciphr2)
 
-  if 1 != subtle.ConstantTimeCompare(result,plain) {
-    t.Errorf("2 DES Dec failed.")
-  }
+	if 1 != subtle.ConstantTimeCompare(result, plain) {
+		t.Errorf("2 DES Dec failed.")
+	}
 
-  c3,_ := NewDES3Cipher(key3)
-  c3.Decrypt(result, ciphr3)
+	c3, _ := NewDES3Cipher(key3)
+	c3.Decrypt(result, ciphr3)
 
-  if 1 != subtle.ConstantTimeCompare(result,plain) {
-    t.Errorf("3 DES Dec failed.")
-  }
+	if 1 != subtle.ConstantTimeCompare(result, plain) {
+		t.Errorf("3 DES Dec failed.")
+	}
 }
 
 
-func TestDec3IsNonMutating (t *testing.T){
-  c3,_ := NewDES3Cipher(key3)
-  enc := make([]byte,8)
-  copy(enc, ciphr3)
-  result := make([]byte,8)
-  c3.Decrypt(result, enc)
+func TestDec3IsNonMutating(t *testing.T) {
+	c3, _ := NewDES3Cipher(key3)
+	enc := make([]byte, 8)
+	copy(enc, ciphr3)
+	result := make([]byte, 8)
+	c3.Decrypt(result, enc)
 
-  if 1 != subtle.ConstantTimeCompare(enc,ciphr3) {
-    t.Errorf("3 DES Dec mutated the cipher string")
-  }
+	if 1 != subtle.ConstantTimeCompare(enc, ciphr3) {
+		t.Errorf("3 DES Dec mutated the cipher string")
+	}
 }
 
 
-func TestSanity (t *testing.T) {
-  if _, error := NewDESCipher(nil); error == nil {
-    t.Errorf("nil key should result in error")
-  }
-  // key too short
-  if _, error := NewDESCipher(oddParityBytes); error == nil {
-    t.Errorf("Incorrect key length should result in error")
-  }
+func TestSanity(t *testing.T) {
+	if _, error := NewDESCipher(nil); error == nil {
+		t.Errorf("nil key should result in error")
+	}
+	// key too short
+	if _, error := NewDESCipher(oddParityBytes); error == nil {
+		t.Errorf("Incorrect key length should result in error")
+	}
 
-  // key des2 too long
-  if _,error := NewDES2Cipher(key3); error == nil {
-    t.Errorf("Incorrect key length should result in error, key too long for 2DES")
-  }
+	// key des2 too long
+	if _, error := NewDES2Cipher(key3); error == nil {
+		t.Errorf("Incorrect key length should result in error, key too long for 2DES")
+	}
 
-  if _, error := NewDESCipher(key_parity_error); error == nil {
-    t.Errorf("Incorrect key length should result in error, was (%d) should be 5", error)
-  }
+	if _, error := NewDESCipher(key_parity_error); error == nil {
+		t.Errorf("Incorrect key length should result in error, was (%d) should be 5", error)
+	}
 
 }
 
-func TestParity (t *testing.T) {
- 
-  for _, b := range(oddParityBytes) {
-    if odd != checkParityByte(b) {
-      t.Errorf("Parity of %X should be odd!", b)
-    }
-  }
+func TestParity(t *testing.T) {
 
-  for _, b := range(evenParityBytes) {
-    if even != checkParityByte(b) {
-      t.Errorf("Parity of %X should be even!", b)
-    }
-  }
+	for _, b := range oddParityBytes {
+		if odd != checkParityByte(b) {
+			t.Errorf("Parity of %X should be odd!", b)
+		}
+	}
+
+	for _, b := range evenParityBytes {
+		if even != checkParityByte(b) {
+			t.Errorf("Parity of %X should be even!", b)
+		}
+	}
 }
 
 
-func TestGenerateKey (t *testing.T) {
-  for i:=0; i!=1000; i++ {
-    // 1DES
-    if key, err := GenerateDESKey(); err != nil {
-      t.Errorf("Error generating key!")
-    } else {
-      if 8 != len(key) {
-        t.Errorf("Wrong key length for generated 1DES key: %d", len(key));
-      }
+func TestGenerateKey(t *testing.T) {
+	for i := 0; i != 1000; i++ {
+		// 1DES
+		if key, err := GenerateDESKey(); err != nil {
+			t.Errorf("Error generating key!")
+		} else {
+			if 8 != len(key) {
+				t.Errorf("Wrong key length for generated 1DES key: %d", len(key))
+			}
 
-      if _, ok := checkParityBytes(key); ok != true {
-        t.Errorf(" 1DES Key with incorrect parity generated!")
-      }
-    } // else
+			if _, ok := checkParityBytes(key); ok != true {
+				t.Errorf(" 1DES Key with incorrect parity generated!")
+			}
+		} // else
 
-    // 2DES
-    if key, err := GenerateDES2Key(); err != nil {
-      t.Errorf("Error generating key!")
-    } else {
-      if 16 != len(key) {
-        t.Errorf("Wrong key length for generated 2DES key: %d", len(key));
-      }
+		// 2DES
+		if key, err := GenerateDES2Key(); err != nil {
+			t.Errorf("Error generating key!")
+		} else {
+			if 16 != len(key) {
+				t.Errorf("Wrong key length for generated 2DES key: %d", len(key))
+			}
 
-      if _, ok := checkParityBytes(key); ok != true {
-        t.Errorf("2DES Key with incorrect parity generated!")
-      }
-    } // else
+			if _, ok := checkParityBytes(key); ok != true {
+				t.Errorf("2DES Key with incorrect parity generated!")
+			}
+		} // else
 
-    // 3DES
-    if key, err := GenerateDES3Key(); err != nil {
-      t.Errorf("Error generating key!")
-    } else {
-      if 24 != len(key) {
-        t.Errorf("Wrong key length for generated 3DES key: %d", len(key));
-      }
+		// 3DES
+		if key, err := GenerateDES3Key(); err != nil {
+			t.Errorf("Error generating key!")
+		} else {
+			if 24 != len(key) {
+				t.Errorf("Wrong key length for generated 3DES key: %d", len(key))
+			}
 
-      if _, ok := checkParityBytes(key); ok != true {
-        t.Errorf("3DES Key with incorrect parity generated!")
-      }
-    } // else
+			if _, ok := checkParityBytes(key); ok != true {
+				t.Errorf("3DES Key with incorrect parity generated!")
+			}
+		} // else
 
-  } // for
+	} // for
 }
-
-
-
-
-
